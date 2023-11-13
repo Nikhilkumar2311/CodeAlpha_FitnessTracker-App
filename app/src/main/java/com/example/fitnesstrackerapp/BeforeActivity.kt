62d60a8c8dda0c15d60a8af14a331f66dc33a5df
Nikhilkumar2311 <120698682+Nikhilkumar2311@users.noreply.github.com>
2023-11-13 13:00:30 +0530

    Before UI

--- app/src/main/java/com/example/fitnesstrackerapp/BeforeActivity.kt
+++ app/src/main/java/com/example/fitnesstrackerapp/BeforeActivity.kt
@@ -2,10 +2,13 @@ package com.example.fitnesstrackerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class BeforeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_before)
     }
+
+    fun Imagebuttonclicked(view: View) {}
 }
